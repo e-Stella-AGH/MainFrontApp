7d4b13c29233150fb814840fd6a281df1cf021a6
Dawid Dębowski <dawid199960@gmail.com>
2021-06-04 19:59:45 +0200

    some css

--- src/components/meeting/MeetingDisplayNameForm.js
+++ src/components/meeting/MeetingDisplayNameForm.js
@@ -1,5 +1,6 @@
 import {useState} from "react";
-import {Button, TextField} from "@material-ui/core";
+import {Box, Button, TextField, Typography} from "@material-ui/core";
+import {colors} from "../../utils/colors";
 
 export const MeetingDisplayNameForm = (props) => {
 
@@ -10,9 +11,27 @@ export const MeetingDisplayNameForm = (props) => {
     }
 
     return (
-        <div>
-            <TextField value={name} onChange={({target}) => setName(target.value)}/>
-            <Button onClick={handleClick}>Submit</Button>
+        <div style={{textAlign: "center"}}>
+            <Box mt={10} mb={5} style={{textAlign: "left", marginLeft: "30%"}}>
+                <Typography variant="h4">
+                    One more step...
+                </Typography>
+            </Box>
+            <Box mb={5} m={2} style={{textAlign: "left", marginLeft: "31%", color: colors["text-gray"]}}>
+                <Typography variant="h5">
+                    How should we call you?
+                </Typography>
+            </Box>
+            <Box mt={5} m={5}>
+                <TextField
+                    value={name}
+                    onChange={({target}) => setName(target.value)}
+                    label="Your name"
+                />
+            </Box>
+            <Box m={5}>
+                <Button variant="contained" color="primary" onClick={handleClick}>Submit</Button>
+            </Box>
         </div>
     )
 }
