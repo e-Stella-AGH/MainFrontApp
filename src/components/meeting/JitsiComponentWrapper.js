fc06c683095f9bb3b597ff647b415f3f5f073b5f
Dawid Dębowski <dawid199960@gmail.com>
2021-06-04 21:14:44 +0200

    whiteboard added

--- src/components/meeting/JitsiComponentWrapper.js
+++ src/components/meeting/JitsiComponentWrapper.js
@@ -1,18 +1,39 @@
 import {JitsiComponent} from "e-stella-jitsi";
 import {MeetingFab} from "./MeetingFab";
+import {useState} from "react";
+import {Whiteboard} from "e-stella-whiteboard";
+import {Grid} from "@material-ui/core";
 
 export const JitsiWrapper = (props) => {
 
+    const [jitsiHeight, setJitsiHeight] = useState('calc(100vh - 60px)')
+    const [jitsiWidth, setJitsiWidth] = useState('100%')
+    const [whiteboardIsSeen, setWhiteboardIsSeen] = useState(false)
+
     const addWhiteboard = () => {
-        alert("added whiteboard")
+        setJitsiWidth('20%')
+        setJitsiHeight('calc(100vh - 60px)')
+        setWhiteboardIsSeen(true)
     }
 
+    const defaultView = () => {
+        setJitsiHeight('calc(100vh-60px)')
+        setJitsiWidth('100%')
+        setWhiteboardIsSeen(false)
+    }
+
+    const getWhiteboardCode = () => {
+        if(props.roomName.length < 15) return 'abcdefghijklmnopqrst'
+        return props.roomName
+    }
 
     return (
-        <div style={{height: 'calc(100vh - 60px)'}}>
-            { props.admin ?
-                <MeetingFab onWhiteboard={addWhiteboard} /> : null }
-            <JitsiComponent admin={props.admin} roomName={props.roomName} displayName={props.displayName} />
+        <div>
+            <MeetingFab onWhiteboard={addWhiteboard} onDefaultView={defaultView} />
+            <div style={{height: jitsiHeight, width: jitsiWidth, float: "left"}}>
+                <JitsiComponent admin={props.admin} roomName={props.roomName} displayName={props.displayName} />
+            </div>
+            { whiteboardIsSeen ? <div style={{float: "left", width: 'calc(80%)', height: 'calc(100vh-60px)'}}><Whiteboard client_id={'ab072386034be36d086179b113d9fb57'} code={getWhiteboardCode()} /></div>: null }
         </div>
     )
 }
--- src/components/meeting/MeetingFab.js
+++ src/components/meeting/MeetingFab.js
@@ -2,6 +2,7 @@ import {Action, Fab} from "react-tiny-fab";
 import AddIcon from '@material-ui/icons/Add';
 import {colors} from "../../utils/colors";
 import DashboardIcon from '@material-ui/icons/Dashboard';
+import VoiceChatIcon from '@material-ui/icons/VoiceChat';
 
 export const MeetingFab = (props) => {
     return(
@@ -10,6 +11,12 @@ export const MeetingFab = (props) => {
             icon={<AddIcon />}
             event={'click'}
         >
+            <Action
+                text="Default View"
+                onClick={props.onDefaultView}
+            >
+                <VoiceChatIcon />
+            </Action>
             <Action
                 text="Whiteboard"
                 onClick={props.onWhiteboard}
