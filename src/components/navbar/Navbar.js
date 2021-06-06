175cd4fdea7ea87b1e1b5aca8474f20a5c32d6a0
Dawid Dębowski <dawid199960@gmail.com>
2021-06-06 21:11:33 +0200

    sb kill me plis

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,7 @@
 import {AppBar, Button, IconButton, makeStyles, Toolbar, Typography} from "@material-ui/core";
 import {colors} from "../../utils/colors";
 import { BrowserRouter as Router, Link } from "react-router-dom";
+import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -13,26 +14,29 @@ const useStyles = makeStyles((theme) => ({
         flexGrow: 1,
     },
 }));
-
 export const Navbar = (props) => {
 
     const classes = useStyles()
 
     return(
-        <AppBar position="static" className={classes.root} style={{ background: colors.main, height: "60px" }}>
+        <AppBar position="static" style={{ background: colors.main, height: "60px" }}>
             <Toolbar>
                 <IconButton edge="start" color="inherit" aria-label="menu" className={classes.menuButton}>
-
+                    <MenuIcon />
                 </IconButton>
                 <Router>
-                    <Link to="/" style={{color: "white", textDecoration: "none"}}>
-                        <Typography variant="h6" className={classes.title}>
-                            e-Stella
-                        </Typography>
-                    </Link>
-                    <Link to="/login" style={{color: "white", textDecoration: "none", float: "right"}}>
-                        <Button color="inherit" id="loginButton">Login</Button>
-                    </Link>
+                    <div>
+                        <Link to="/" style={{color: "white", textDecoration: "none"}}>
+                            <Typography variant="h6" className={classes.title}>
+                                e-Stella
+                            </Typography>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to="/login" style={{color: "white", textDecoration: "none"}}>
+                            <Button color="inherit" id="loginButton">Login</Button>
+                        </Link>
+                    </div>
                 </Router>
             </Toolbar>
         </AppBar>
