f97c86e5fb1edd9bc828d3f6c5831262cf72ff54
Dawid Dębowski <dawid199960@gmail.com>
2021-06-29 17:58:25 +0200

    delete login and password

--- src/utils/apis/LoginAPI.js
+++ src/utils/apis/LoginAPI.js
@@ -1,7 +1,6 @@
 export const loginAPI = {
 
     login: function(login, password) {
-        console.log(login, password)
         return new Promise(((resolve, reject) => {
             setTimeout(() => {
                 resolve("Token")
