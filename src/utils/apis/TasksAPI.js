e256d61f24826487fec57289cf383e826a489432
Rados13 <radoslawszuma@gmail.com>
2021-09-21 23:32:55 +0200

    Add proper TasksAPI

---
+++ src/utils/apis/TasksAPI.js
@@ -0,0 +1,29 @@
+import { recruitmentServiceBasicAPILink } from "./APILinks";
+import Swal from 'sweetalert2'
+
+export const tasksApi = {
+
+    getTask: (processId) => {
+        if (processId) {
+            return fetch(`${recruitmentServiceBasicAPILink}/api/tasks?process=${processId}`)
+                .then(response => response.json())
+        } else {
+            Swal.fire({
+                title: 'Oops!',
+                icon: 'error',
+                text: `Looks like somebody didn't add a task but wants to solve one something!`
+            })
+            return new Promise(_ => { })
+        }
+    },
+
+    codeCheckerLink: "https://e-stella-code-executor.herokuapp.com",
+
+    sendTestResult: (body) => {
+        return fetch(`${recruitmentServiceBasicAPILink}/api/tasks/taskResult?taskId=${body.task.id}&processId=${body.id}`, {
+            method: "POST",
+            body: JSON.stringify(body)
+        })
+    }
+
+}
